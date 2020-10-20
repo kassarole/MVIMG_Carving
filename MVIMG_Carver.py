131172eeec9037a906a296b36cc595ff1b1ff711
Kevin Rode <krode17@gmail.com>
2020-10-19 20:17:59 -0400

    Initial Commit

---
+++ MVIMG_Carver.py
@@ -0,0 +1,19 @@
+import os, exiftool
+for file in os.listdir(os.getcwd()):
+    try:
+        with exiftool.ExifTool() as et:
+            offset = et.get_tag('MicroVideoOffset', file)
+        if offset != None and offset != '' :
+            print(offset)
+            size = os.path.getsize(file)
+            print(offset)
+            offset = int(size)-int(offset)
+            with open(file, "rb") as infile:
+                infile.seek(int(offset))
+                data = infile.read()
+            with open(file+".mp4", "wb") as outfile:
+                outfile.write(data)
+        else:
+            pass
+    except ValueError:
+        pass
---
+++ README.md
@@ -0,0 +1,8 @@
+# Motion Photo Carver
+This tool is designed to carve out the video segment of Motion Videos from an android device. It is written in python 2.7 to ease porting it to the Magnet Axiom Artifact Exchange.
+## Requirements
+* Python 2.7
+* PyExifTool \(Found [here](https://github.com/smarnach/pyexiftool)\)
+## Usage
+Begin by running `pip install -r requirements.txt` \
+The script can then be used by placing it in the same directory as extracted MVIMG files and running `python MVIMG_Carver.py`
